bb4eaf1e2907843bbb0e68a1249662af4ef9e44d
Herik Wang <HerikkWang@github.com>
2018-12-20 00:06:06 +0800

    create web application ApplyController

---
+++ ApplyController/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.core.validators import MinLengthValidator, RegexValidator, MaxValueValidator, MinValueValidator
+
+
+class Merchant(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=20)
+    Password = models.CharField(max_length=20)
+    IdentityNum = models.CharField(max_length=18, validators=MinLengthValidator(20))
+
+    PhoneNum_regex_validator = RegexValidator(regex=r'^\d{7}$', inverse_match=True)
+    PhoneNum = models.CharField(max_length=7, validators=PhoneNum_regex_validator)
+
+    def __str__(self):
+        return self.Name
+
+
+def get_file_path(instance):
+    # 设置Restaurant的Image的上传路径
+    return 'files/image/%s/%s' % (instance.MerchantID.code, instance.Name.code)
+
+
+class Restaurant(models.Model):
+    ID = models.AutoField(primary_key=True)
+    MerchantID = models.ForeignKey(Merchant, on_delete=models.CASCADE)
+    Name = models.CharField(max_length=20)
+    BusinessStartHour = models.TimeField()
+    BusinessEndHour = models.TimeField()
+    Address = models.CharField(max_length=50)
+    Image = models.ImageField(upload_to=get_file_path)
+    Score = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+
+    Category_Choices = (
+        ('1', '川菜'),
+        ('2', '粤菜'),
+        ('3', '鲁菜'),
+        ('4', '苏菜'),
+        ('5', '湘菜'),
+        ('6', '浙菜'),
+        ('7', '其他'),
+    )
+    Category = models.CharField(max_length=1, choices=Category_Choices)
+    LicenseID = models.CharField(validators=RegexValidator(regex=r'^[0-9]*$', inverse_match=True))
+    ApplicationTime = models.DateTimeField(auto_now_add=True)
+
+    Status_Choices = (
+        (0, '待审核'),
+        (1, '审核拒绝'),
+        (2, '审核通过'),
+    )
+    Status = models.SmallIntegerField(choices=Status_Choices)
+    OpenTime = models.DateTimeField(null=True)
+
